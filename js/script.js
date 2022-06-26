a1ace1ff5b66dcd13e18c4f087f1e8585c7bae24
TairSadyk <tairsadykov92@gmail.com>
2022-06-26 14:42:24 -0400

    Final desktop version of main page

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// document.querySelector('body').addEventListener('mousemove', function (e) {
+//   [...document.querySelectorAll('.illustration__iris')].forEach(iris => {
+//     let x = iris.getBoundingClientRect().left + iris.clientWidth / 2;
+//     let y = iris.getBoundingClientRect().top + iris.clientHeight / 2;
+//     // let radian = Math.atan2(e.pageX - x, e.pageY - y);
+//     // let rot = radian * (180 / Math.PI) * -1 + 270;
+//     iris.style.transform = `translate(${x}%,${y}%)`;
+//   });
+// });
